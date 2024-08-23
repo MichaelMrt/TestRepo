b35963deeb09883700b3c57e4cdbe24805c7190d
Michael <michael.martynov03@gmail.com>
2024-08-23 19:42:56 +0200

    skript erweitert und gitignore angepasst

--- skript.py
+++ skript.py
@@ -1,4 +1,10 @@
-import time
 print("hello world")
 with open("test.txt", 'w') as file:
     file.write('hi')
+
+with open("test2.txt", 'w') as file:
+    file.write('hi')
+
+with open("test3.txt", 'w') as file:
+    file.write('hi')
+
